5e85d4c5458db852059fd9c8d107eec155ad23f2
Shikesh Jayan <shikeshjayan345@gmail.com>
2025-10-16 15:46:05 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Simple Grade Calculator with Conditional Statements
+// Input from User
+let subjectEnglish = Number(prompt("Enter Mark of English: "));
+let subjectMaths = Number(prompt("Enter Mark of Mathemathics: "));
+let subjectPhysics = Number(prompt("Enter Mark of Physics: "));
+let subjectChemistry = Number(prompt("Enter Mark of Chemistry: "));
+let subjectBiology = Number(prompt("Enter Mark of Biology: "));
+// Display Subjects and It's Marks
+// console.log(
+//   `Your Marks are: English : ${subjectEnglish}, Maths : ${subjectMaths}, Physics : ${subjectPhysics}, Chemistry : ${subjectChemistry}, Biology : ${subjectBiology}`
+// );
+// Calculating Total Marks
+const totalMark =
+  subjectEnglish +
+  subjectMaths +
+  subjectPhysics +
+  subjectChemistry +
+  subjectBiology;
+//   Display Total Marks
+// console.log("Total Mark is " + totalMark);
+const average = totalMark / 5;
+// Computing the Average Mark
+// console.log("Average Mark is " + average);
+// Grade Calculator
+let grade;
+if (average >= 90) {
+  grade = "A+";
+} else if (average >= 80) {
+  grade = "A";
+} else if (average >= 70) {
+  grade = "B";
+} else if (average >= 60) {
+  grade = "C";
+} else if (average >= 50) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+// Display Grade
+// console.log("Grade is " + grade);
+alert(`Total Marks: ${totalMark}\nAverage: ${average}\nGrade: ${grade}`);
